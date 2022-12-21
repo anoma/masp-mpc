b89934a89863c51db4c25970c033f669f7868032
joe bebel <55120843+joebebel@users.noreply.github.com>
2022-12-21 04:20:30 -0800

    btc eth zec hash for beacon

--- src/bin/beacon.rs
+++ src/bin/beacon.rs
@@ -9,14 +9,16 @@ use std::io::Write;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 5 {
-        println!("Usage: \n<in_params.params> <in_beacon_hash> <in_num_iterations_exp> <out_params.params>");
+    if args.len() != 7 {
+        println!("Usage: \n<in_params.params> <in_btc_hash> <in_eth_hash> <in_zec_hash> <in_num_iterations_exp> <out_params.params>");
         std::process::exit(exitcode::USAGE);
     }
     let in_params_filename = &args[1];
-    let beacon_hash = &args[2];
-    let num_iterations_exp = &args[3].parse::<usize>().unwrap();
-    let out_params_filename = &args[4];
+    let btc_hash = &args[2];
+    let eth_hash = &args[3];
+    let zec_hash = &args[4];
+    let num_iterations_exp = &args[5].parse::<usize>().unwrap();
+    let out_params_filename = &args[6];
     let print_progress = true;
 
     if *num_iterations_exp < 10 || *num_iterations_exp > 63 {
@@ -33,13 +35,26 @@ fn main() {
         use rand_chacha::ChaChaRng;
         use std::convert::TryInto;
 
-        // The hash used for the beacon
-        let hash_result = hex::decode(beacon_hash);
-        if hash_result.is_err() {
-            println!("Beacon hash should be in hexadecimal format");
-            std::process::exit(exitcode::DATAERR);
-        }
-        let mut cur_hash = hash_result.unwrap();
+        let mut cur_hash = {
+            let mut h = Sha256::new();
+
+            for beacon_hash in [btc_hash, eth_hash, zec_hash] {
+                // The hash used for the beacon
+                let hash_result = hex::decode(beacon_hash);
+                if hash_result.is_err() {
+                    println!("Beacon hash should be in hexadecimal format");
+                    std::process::exit(exitcode::DATAERR);
+                }
+                let hash_result = hash_result.unwrap();
+                if hash_result.len() != 32 {
+                    println!("Beacon hash should be 32 bytes long");
+                    std::process::exit(exitcode::DATAERR);
+                }
+                h.update(&hash_result);
+            }
+            h.finalize().to_vec()
+        };
+
         if cur_hash.len() != 32 {
             println!("Beacon hash should be 32 bytes long");
             std::process::exit(exitcode::DATAERR);
@@ -85,13 +100,13 @@ fn main() {
     reader.seek_relative(64).unwrap();
     let mut spend_params = MPCParameters::read(&mut reader, false).expect("unable to read params");
 
-    println!("Contributing to Spend {}...", in_params_filename);
+    println!("Contributing to MASP Spend {}...", in_params_filename);
 
     let spend_hash = spend_params.contribute(&mut rng, &0);
 
     let mut output_params = MPCParameters::read(&mut reader, false).expect("unable to read params");
 
-    println!("Contributing to Output {}...", in_params_filename);
+    println!("Contributing to MASP Output {}...", in_params_filename);
 
     let output_hash = output_params.contribute(&mut rng, &0);
 
@@ -118,9 +133,9 @@ fn main() {
     println!("Contribution hash: 0x{:02x}", h.iter().format(""));
 
     let mut f = File::create(out_params_filename).unwrap();
-for _ in 0..64 {
-    f.write_all(&[0u8]);
-}
+    for _ in 0..64 {
+        f.write_all(&[0u8]).unwrap();
+    }
     println!("Writing MASP Spend parameters to {}.", out_params_filename);
     spend_params
         .write(&mut f)
